d4b5b09b94838684c3d19fd0ac8aaf4e2d8ea1cd
EdoardoFurlanUni <edoardo.furlan.3@studenti.unipd.it>
2025-08-30 12:40:56 +0200

    messo upsampling + downsampling, funziona ma male

--- main.cpp
+++ main.cpp
@@ -77,34 +77,50 @@ int main(int argc, const char* argv[])
     //     }
     // }
 
-    // compute circles locations and split gray-scale test images in smaller images containig coins
+    // compute circles locations and split gray-scale test images in smaller images containing coins
     // then preprocess those images
-    std::vector<std::vector<cv::Vec3f>> circles_positions;
-    std::vector<std::vector<cv::Mat>> preprocessed_test_images_coins;
-    preprocessed_test_images_coins.reserve(test_images_gray.size());
+    cv::VideoCapture cap("../test/videos/video2.MOV");
+    if (!cap.isOpened()) {
+        std::cerr << "Errore: impossibile aprire il video!" << std::endl;
+        return -1;
+    }
 
-    for (size_t i = 0; i < test_images_gray.size(); i++) {
+    cv::Mat frame;
+    cv::namedWindow("Template Matching", cv::WINDOW_KEEPRATIO);
+
+    while (true) {
+        cap >> frame;
+        if (frame.empty()) break;  // fine del video
+        std::vector<std::vector<cv::Mat>> preprocessed_test_images_coins;
+        //preprocessed_test_images_coins.reserve(test_images_gray.size());
+        cv::Mat frame_gray;
+        cv::resize(frame, frame, cv::Size(3024, 4032));
+        cv::resize(frame, frame, cv::Size(), downsampling_factor, downsampling_factor);
+        std::vector<std::vector<cv::Vec3f>> circles_positions;
+
+        cv::cvtColor(frame, frame_gray, cv::COLOR_BGR2GRAY);
+
         cv::Mat img_HSV;
-        cv::cvtColor(test_images_colour[i], img_HSV, cv::COLOR_BGR2HSV);
+        cv::cvtColor(frame, img_HSV, cv::COLOR_BGR2HSV);
 
         cv::Mat img;
-        
-        std::vector<cv::Vec3f> circles = get_circles_positions(test_images_colour[i], downsampling_factor);
+
+        std::vector<cv::Vec3f> circles = get_circles_positions(frame, downsampling_factor);
 
         //std::vector<cv::Vec3f> circles = get_circles_positions(img_HSV, downsampling_factor);
         circles_positions.push_back(circles);
 
-        std::vector<cv::Mat> coin_images = split_image_by_coins(test_images_gray[i], circles, coin_image_margin);
+        std::vector<cv::Mat> coin_images = split_image_by_coins(frame_gray, circles, coin_image_margin);
         std::vector<cv::Mat> preprocessed_coin_images = preprocess_images(coin_images, points_contrast_stretching, gaussian_kernel_size, gaussian_kernel_sigma);
         preprocessed_test_images_coins.push_back(preprocessed_coin_images);
 
         // // Print number of circles found *****
-        // std::cout << "Found " << circles.size() << " circles in the image." << std::endl;
-
+        std::cout << "Found " << preprocessed_test_images_coins.size() << " circles in the image." << std::endl;
+        //cv::waitKey(0);
         // // Print radius of the circles *****
-        // for (size_t j = 0; j < circles.size(); j++) {
-        //     std::cout << "Circle " << j << ": radius = " << circles[j][2] << std::endl;
-        // }
+        for (size_t j = 0; j < circles.size(); j++) {
+             std::cout << "Circle " << j << ": radius = " << circles[j][2] << std::endl;
+        }
 
         // Draw circles on the original image *****
         for (size_t j = 0; j < circles.size(); j++) {
@@ -113,8 +129,8 @@ int main(int argc, const char* argv[])
             cv::Point center(cvRound(circle[0]), cvRound(circle[1]));
             int radius = cvRound(circle[2]);
 
-            cv::circle(test_images_colour[i], center, radius, cv::Scalar(255, 0, 255), static_cast<int>(3*downsampling_factor), cv::LINE_AA);
-            cv::putText(test_images_colour[i], std::to_string(j), center, cv::FONT_HERSHEY_SIMPLEX, 1.5*downsampling_factor, cv::Scalar(255, 0, 255), static_cast<int>(3*downsampling_factor));
+            cv::circle(frame, center, radius, cv::Scalar(255, 0, 255), static_cast<int>(3*downsampling_factor), cv::LINE_AA);
+            cv::putText(frame, std::to_string(j), center, cv::FONT_HERSHEY_SIMPLEX, 1.5*downsampling_factor, cv::Scalar(255, 0, 255), static_cast<int>(3*downsampling_factor));
 
             // // Show circles on original image *****
             // cv::namedWindow("Hough Circles", cv::WINDOW_KEEPRATIO);
@@ -122,122 +138,120 @@ int main(int argc, const char* argv[])
             // cv::waitKey(0);
 
             // // Show coin images *****
-            // cv::namedWindow("Coin", cv::WINDOW_KEEPRATIO);
-            // cv::imshow("Coin", preprocessed_coin_images[j]);
-            // cv::waitKey(0);
+            //cv::namedWindow("Coin", cv::WINDOW_KEEPRATIO);
+            //cv::imshow("Coin", preprocessed_coin_images[j]);
+            //cv::waitKey(0);
 
             // // Show size of coin image *****
             // cv::Size coin_size = preprocessed_coin_images[j].size();
             // std::cout << "Coin " << j << " size: " << coin_size.width << "x" << coin_size.height << std::endl;
-        }
-    }    
+        }    
 
 
-    // ----- TEMPLATE MATCHING (test) -----
-    std::vector<std::vector<DetectedCoin>> predicted_labels;
+        // ----- TEMPLATE MATCHING (test) -----
+        std::vector<std::vector<DetectedCoin>> predicted_labels;
 
-    cv::namedWindow("Template Matching", cv::WINDOW_KEEPRATIO);
-
-    // loop over all test images
-    for (size_t i = 0; i < preprocessed_test_images_coins.size(); i++) {
-        const std::vector<cv::Mat>& coin_images = preprocessed_test_images_coins[i];
-
-        auto start = std::chrono::high_resolution_clock::now();
-
-        // list of detected coins with reference to the whole test image
         std::vector<DetectedCoin> coins_found;     // center, radius, confidence, class
 
-        // loop over all coins sub-images
-        for (size_t j = 0; j < coin_images.size(); j++) {
-            cv::Mat coin_img = coin_images[j];
+        // loop over all test images
+        for (size_t i = 0; i < preprocessed_test_images_coins.size(); i++) {
+            //std::cout << "Tipo dell'immagine " << i << ": " << typeid(preprocessed_test_images_coins[i]).name() << std::endl;
+            const std::vector<cv::Mat>& coin_images = preprocessed_test_images_coins[i];
 
-            // best match with reference to the coin image
-            DetectedCoin best_match;
-            best_match.confidence = -1.0; // initialization
+            auto start = std::chrono::high_resolution_clock::now();
 
-            // matching over all templates
-            for (size_t c = 0; c < coins_classes.size(); c++) {
-                for (const cv::Mat& template_img : preprocessed_dataset_images[c]) {
+            // list of detected coins with reference to the whole test image
 
-                    // if template is bigger than coin image, skip matching
-                    if (template_img.cols > coin_img.cols || template_img.rows > coin_img.rows) {
-                        // // Show skipped template *****
-                        // std::cout << "Skipping template of class " << coins_classes[c] << " of size " << template_img.size() << " for coin image " << j << " of size " << coin_img.size() << std::endl;
-                        continue;
-                    }
+            // loop over all coins sub-images
+            for (size_t j = 0; j < coin_images.size(); j++) {
+                cv::Mat coin_img = coin_images[j];
 
-                    // rotate template
-                    std::vector<cv::Mat> rotations = rotate_template(template_img, 8);
-                    for (const cv::Mat& rotated_template : rotations) {
+                // best match with reference to the coin image
+                DetectedCoin best_match;
+                best_match.confidence = -1.0; // initialization
+                // matching over all templates
+                for (size_t c = 0; c < coins_classes.size(); c++) {
+                    for (const cv::Mat& template_img : preprocessed_dataset_images[c]) {
 
-                        cv::Mat result;
+                        // if template is bigger than coin image, skip matching
+                        if (template_img.cols > coin_img.cols || template_img.rows > coin_img.rows) {
+                            // // Show skipped template *****
+                            // std::cout << "Skipping template of class " << coins_classes[c] << " of size " << template_img.size() << " for coin image " << j << " of size " << coin_img.size() << std::endl;
+                            continue;
+                        }
 
-                        cv::matchTemplate(coin_img, rotated_template, result, cv::TM_CCOEFF_NORMED);
-                        
-                        DetectedCoin current_match = get_best_match_above_threshold(result, 0.5, template_img.cols, coins_classes[c]);
-                        // // Print current match confidence *****
-                        // std::cout << "Current match confidence: " << current_match.confidence << std::endl;
+                        // rotate template
+                        std::vector<cv::Mat> rotations = rotate_template(template_img, 8);
+                        for (const cv::Mat& rotated_template : rotations) {
 
-                        if (current_match.confidence > best_match.confidence) {
-                            best_match = current_match;
+                            cv::Mat result;
+
+                            cv::matchTemplate(coin_img, rotated_template, result, cv::TM_CCORR_NORMED);
+
+                            DetectedCoin current_match = get_best_match_above_threshold(result, 0.9, template_img.cols, coins_classes[c]);
+                            // // Print current match confidence *****
+                            // std::cout << "Current match confidence: " << current_match.confidence << std::endl;
+
+                            if (current_match.confidence > best_match.confidence) {
+                                best_match = current_match;
+                            }
                         }
                     }
                 }
+                // // Print best match confidence *****
+                // std::cout << "Best match confidence: " << best_match.confidence << std::endl; 
+                // if a match was found convert it from coin reference frame to test image frame
+                if (best_match.confidence > 0) {
+
+                    // // Show circle found in coin image *****
+                    // cv::namedWindow("Coin", cv::WINDOW_KEEPRATIO); 
+                    // cv::cvtColor(coin_img, coin_img, cv::COLOR_GRAY2BGR);
+                    // cv::circle(coin_img, best_match.center, best_match.radius, cv::Scalar(0, 255, 0), 5);
+                    // cv::imshow("Coin", coin_img);
+                    // cv::waitKey(0);
+                    best_match.center += cv::Point(std::max(0.0f, circles_positions[i][j][0] - circles_positions[i][j][2] - coin_image_margin), std::max(0.0f, circles_positions[i][j][1] - circles_positions[i][j][2] - coin_image_margin));
+
+                    // // Show circle found in whole image *****
+                    // cv::namedWindow("whole image", cv::WINDOW_KEEPRATIO);
+                    // cv::circle(test_images_colour[i], best_match.center, best_match.radius, cv::Scalar(0, 255, 0), 5);
+                    // cv::imshow("whole image", test_images_colour[i]);
+                    // cv::waitKey(0);
+
+                    coins_found.push_back(best_match);
+                }
             }
 
-            // // Print best match confidence *****
-            // std::cout << "Best match confidence: " << best_match.confidence << std::endl; 
-            // if a match was found convert it from coin reference frame to test image frame
-            if (best_match.confidence > 0) {
+            // Store predicted labels for each image
+            predicted_labels.push_back(coins_found);
 
-                // // Show circle found in coin image *****
-                // cv::namedWindow("Coin", cv::WINDOW_KEEPRATIO); 
-                // cv::cvtColor(coin_img, coin_img, cv::COLOR_GRAY2BGR);
-                // cv::circle(coin_img, best_match.center, best_match.radius, cv::Scalar(0, 255, 0), 5);
-                // cv::imshow("Coin", coin_img);
-                // cv::waitKey(0);
+            // Measure the time taken for template matching
+            auto end = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<double> elapsed = end - start;
+            //std::cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
 
-                best_match.center += cv::Point(std::max(0.0f, circles_positions[i][j][0] - circles_positions[i][j][2] - coin_image_margin), std::max(0.0f, circles_positions[i][j][1] - circles_positions[i][j][2] - coin_image_margin));
-                
-                // // Show circle found in whole image *****
-                // cv::namedWindow("whole image", cv::WINDOW_KEEPRATIO);
-                // cv::circle(test_images_colour[i], best_match.center, best_match.radius, cv::Scalar(0, 255, 0), 5);
-                // cv::imshow("whole image", test_images_colour[i]);
-                // cv::waitKey(0);
-
-                coins_found.push_back(best_match);
+            // Show all labels on the test image *****
+            for (const auto& d : coins_found) {
+                cv::circle(frame, d.center, d.radius, cv::Scalar(0, 255, 0), static_cast<int>(5*downsampling_factor), cv::LINE_AA);
+                cv::putText(frame, d.class_name, cv::Point(d.center.x, d.center.y - 10), cv::FONT_HERSHEY_SIMPLEX, 2*downsampling_factor, cv::Scalar(0, 255, 0), static_cast<int>(5*downsampling_factor));
             }
         }
-
-        // Store predicted labels for each image
-        predicted_labels.push_back(coins_found);
-
-        // Measure the time taken for template matching
-        auto end = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed = end - start;
-        std::cout << "Elapsed time: " << elapsed.count() << " seconds" << std::endl;
-
-        // Show all labels on the test image *****
-        for (const auto& d : coins_found) {
-            cv::circle(test_images_colour[i], d.center, d.radius, cv::Scalar(0, 255, 0), static_cast<int>(5*downsampling_factor), cv::LINE_AA);
-            cv::putText(test_images_colour[i], d.class_name, cv::Point(d.center.x, d.center.y - 10), cv::FONT_HERSHEY_SIMPLEX, 2*downsampling_factor, cv::Scalar(0, 255, 0), static_cast<int>(5*downsampling_factor));
-        }
-        cv::imshow("Template Matching", test_images_colour[i]);
+        cv::imshow("Template Matching", frame);
         std::cout << "number of matches: " << coins_found.size() << std::endl;
-        cv::waitKey(0);
+        // cv::waitKey(0);
+        // Esc per uscire
+        char c = (char)cv::waitKey(25);
+        if (c == 27) break;
+
+        // ----- PERFORMANCE METRICS (test) -----
+        
+        std::vector<std::vector<DetectedCoin>> ground_truth_labels = get_labels_from_folder(test_labels_path, downsampling_factor);
+
+        //for (size_t i = 0; i < ground_truth_labels.size(); ++i) {
+            //float mIoU = compute_mIoU(frame, predicted_labels[i]);
+            //std::cout << "Mean IoU for image " << i << ": " << mIoU << std::endl;
+        //}
+        std::cerr << "Fine video" << std::endl;
     }
-
-
-    // ----- PERFORMANCE METRICS (test) -----
-    
-    std::vector<std::vector<DetectedCoin>> ground_truth_labels = get_labels_from_folder(test_labels_path, downsampling_factor);
-
-    for (size_t i = 0; i < ground_truth_labels.size(); ++i) {
-        float mIoU = compute_mIoU(ground_truth_labels[i], predicted_labels[i]);
-        std::cout << "Mean IoU for image " << i << ": " << mIoU << std::endl;
-
-    }
-
     return 0;
 }
 
